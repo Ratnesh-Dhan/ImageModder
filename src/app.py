cb85e06d2920eb1a63a81fb63df80d9f7a683c9b
Ratnesh-Dhan <ratneshdhan@gmail.com>
2025-01-17 18:03:10 +0530

    putting icon on buttons

--- src/app.py
+++ src/app.py
@@ -7,6 +7,7 @@ from src.functions.filters import CustomFilters
 from src.functions.graphs import Graphs
 from src.functions.file import File
 from src.functions.edit import Edit
+# from PIL import Image, ImageTk
 
 class App:
     def __init__(self, root, menubar, custom_font, label_font, button_font, menu_font):
@@ -18,8 +19,8 @@ class App:
         
         
         self.style = ttk.Style(root)
-        self.style.configure('TButton', background="#F6F4F0", foreground="#2E5077", font=('Helvetica', 10, 'bold'))
-        self.style.configure('TButtonClicked', background="#2E5077", foreground="#F6F4F0", font=('Helvetica', 10, 'bold'))
+        # self.style.configure('TButton', background="#F6F4F0", foreground="#2E5077", font=('Helvetica', 10, 'bold'))
+        # self.style.configure('TButtonClicked', background="#2E5077", foreground="#F6F4F0", font=('Helvetica', 10, 'bold'))
         
         #Creating and packing frames 
         self.topFrame = tk.Frame(self.root, background="#79D7BE", height=100 )
@@ -43,15 +44,25 @@ class App:
         # self.image_label = tk.Label(self.bottomFrame)
         # self.image_viewer = ImageViewer()
         
+        #button icons
+        # hand = Image.open("src/public/hand.png")
+        # grab = Image.open("src/public/hold.png")
+        # color = Image.open("src/public/color.jpg")
+        # hand = hand.resize((50, 20), Image.LANCZOS)
+        # grab = grab.resize((10, 10), Image.LANCZOS)
+        hand_image = tk.PhotoImage("src/public/hand.png")
+        # hand_image = hand_image.subsample(3, 3)
+
         #square buttons
-        self.button_hand = ttk.Button(self.topFrame, text="hand", command=self.image_control.toggle_drag)
+        self.button_hand = ttk.Button(self.topFrame, image=hand_image, compound=tk.LEFT, command=self.image_control.toggle_drag)
+        self.button_grab = ttk.Button(self.topFrame, image=hand_image, compound=tk.LEFT, command=self.image_control.toggle_drag).grid(row=1, column=3, ipadx=0, ipady=10)
         self.button_hand.bind("<Button-1>", self.button_color)
         self.button_hand.grid(row=1, column=0, ipadx=0, ipady=10)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
         ttk.Button(self.topFrame, text="textOut", command=self.image_control.test_output).grid(row=1, column=1, ipadx=0, ipady=10)
         
         
     def on_closing(self):
-        self.root.destroy()
+        self.root.destroy() 
         sys.exit("Quiting..")
         
     def button_color(self, event):
